07071d0bf1b75fc293c20b2b35a9ba0b17afc0fc
R‎‎‎‎‎‎ ‎‎   ‎‎‎‎‎ <ramintac7@gmail.com>
2025-09-30 21:56:10 +0300

    feat: added a function that checks if a user is found

--- src/users/services/find-user-by-email.service.ts
+++ src/users/services/find-user-by-email.service.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { User } from 'src/users/entities/user.entity';
 import { UserRepositoryInterface } from 'src/users/repositories/user-repository.interface';
 import {
@@ -5,13 +6,15 @@ import {
   FindUserByEmailUseCase,
 } from 'src/users/services/usecases/find-user-by-email.usecase';
 
-export class FindByEmailService implements FindUserByEmailUseCase {
+export class FindUserByEmailService implements FindUserByEmailUseCase {
   constructor(private readonly userRepository: UserRepositoryInterface) {}
 
   async execute(payload: FindUserByEmailPort): Promise<User> {
     const { email } = payload;
 
-    const user: User = await this.userRepository.findByEmail(email);
+    const user: User | null = await this.userRepository.findByEmail(email);
+
+    if (user == null) throw new NotFoundException();
 
     return user;
   }
