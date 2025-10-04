6945325acca5a99c1464c4dd45a7f4d432e924d0
Mantrimas <mantrimas.kasparavicius@gmail.com>
2025-10-04 17:49:34 +0300

    [IWH-38](feat): implement password hashing
    implemented password hashing in register user service,
    installed bcrypt type package
    
    Ticket: https://iwashere.atlassian.net/browse/IWH-38

--- src/users/services/register-user.service.ts
+++ src/users/services/register-user.service.ts
@@ -4,17 +4,24 @@ import {
   RegisterUserPort,
   RegisterUserUseCase,
 } from './usecases/register-user.usecase';
+import * as bcrypt from 'bcrypt';
 
 export class RegisterUserService implements RegisterUserUseCase {
   constructor(private readonly userRepository: UserRepositoryInterface) {}
 
   async execute(payload: RegisterUserPort): Promise<User> {
     const { email, username, password } = payload;
+
     const user = new User();
     user.email = email;
     user.username = username;
-    user.password = password;
+    user.password = await this.hashPassword(password);
 
     return await this.userRepository.save(user);
+  }
+
+  private async hashPassword(password: string): Promise<string> {
+    const hash: string = await bcrypt.hash(password, 10);
+    return hash;
   }
 }
