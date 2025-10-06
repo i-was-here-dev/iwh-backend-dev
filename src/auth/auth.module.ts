25500495d405672c8044474e3ce317dfa42a1717
Mantrimas <mantrimas.kasparavicius@gmail.com>
2025-10-06 19:01:47 +0300

    [IWH-47](feat): import jwt service to auth module
    imported jwt module to auth module which provides jwt service
    
    Ticket: https://iwashere.atlassian.net/browse/IWH-47

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,17 @@
 import { Module, Provider } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 
 const repositoryProvider: Array<Provider> = [];
 
 const serviceProvider: Array<Provider> = [];
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: 3000 },
+    }),
+  ],
   controllers: [],
   providers: [...serviceProvider, ...repositoryProvider],
 })
