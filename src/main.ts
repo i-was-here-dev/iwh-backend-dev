b2e175782910057cd7ab729bbdf152f5be37a56f
Mantrimas <mantrimas.kasparavicius@gmail.com>
2025-10-09 18:54:19 +0300

    [IWH-52](feat): update global validation pipe error handling
    created exception facotry method for global validation pipe which return
    `Bad Request` exception if dto property is missing and `Unprocessable
    entity` exception if a property does not adhere to the specified
    requirements,
    
    this method also prevents mutiple error messages to be sent, only the
    first one to be catched will be availabe in the response object.
    
    Ticket: https://iwashere.atlassian.net/browse/IWH-52

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { BadRequestException, UnprocessableEntityException, ValidationError, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, stopAtFirstError: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      stopAtFirstError: true,
+      exceptionFactory: (validationErrors: ValidationError[]) => {
+        const firstError: ValidationError = validationErrors[0];
+        if (Object.prototype.hasOwnProperty.call(firstError.constraints, 'isNotEmpty')) {
+          return new BadRequestException('Required parameters are missing');
+        }
+
+        return new UnprocessableEntityException(Object.values(firstError.constraints)[0]);
+      },
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
