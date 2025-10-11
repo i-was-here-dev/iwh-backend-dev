ace593d8852e71faa20b4c3060db20e24cb5df70
R‎‎‎‎‎‎ ‎‎   ‎‎‎‎‎ <ramintac7@gmail.com>
2025-10-11 14:28:36 +0300

    [IWH-50](feat): create blacklisted token repository
    Created blacklisted token repository
    
    Ticket: https://iwashere.atlassian.net/browse/IWH-50

---
+++ src/auth/repositories/postgres/blacklisted-token.repository.ts
@@ -0,0 +1,17 @@
+import { BlacklistedTokenRepositoryInterface } from 'src/auth/repositories/blacklisted-token-repository.interface';
+import { BlacklistedToken } from '../../entities/token.entity';
+import { Repository } from 'typeorm';
+
+export class BlacklistedTokenRepository implements BlacklistedTokenRepositoryInterface {
+  constructor(private readonly repository: Repository<BlacklistedToken>) {}
+
+  async save(token: BlacklistedToken): Promise<BlacklistedToken> {
+    return await this.repository.save(token);
+  }
+
+  async findByToken(token: string): Promise<BlacklistedToken | null> {
+    return await this.repository.findOne({
+      where: { token: token },
+    });
+  }
+}
