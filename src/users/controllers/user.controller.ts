46dc568acdd24a9334355e14acc0453e036f97d9
Mantrimas <mantrimas.kasparavicius@gmail.com>
2025-10-13 13:17:25 +0300

    [IWH-59](chore): remove register user endpoint from users module
    removed register user endpoint from users controller and removed the
    correlated dtos
    
    Ticket: https://iwashere.atlassian.net/browse/IWH-59

--- src/users/controllers/user.controller.ts
+++ src/users/controllers/user.controller.ts
@@ -1,41 +1,15 @@
-import { Controller, Inject, Post, Body, Get, Param } from '@nestjs/common';
+import { Controller, Inject, Get, Param } from '@nestjs/common';
 import { User } from '../entities/user.entity';
-import { NotFoundException } from '@nestjs/common';
 import { UsersDiTokens } from '../di/users-tokens.di';
-import { SaveUserUseCase } from '../services/usecases/save-user.usecase';
-import { RegisterUserResponseDto } from '../dto/register-user-response.dto';
-import { RegisterUserRequestDto } from '../dto/register-user-request.dto';
 import { FindUserByUuidPort, FindUserByUuidUseCase } from '../services/usecases/find-user-by-uuid.usecase';
 import { FindUserByUuidResponseDto } from '../dto/find-user-by-uuid-response.dto';
 
 @Controller('users')
 export class UserController {
   constructor(
-    @Inject(UsersDiTokens.SaveUserService)
-    private readonly registerUserService: SaveUserUseCase,
     @Inject(UsersDiTokens.FindUserByUuidService)
     private readonly findUserByUuidService: FindUserByUuidUseCase,
   ) {}
-
-  @Post()
-  async register(@Body() dto: RegisterUserRequestDto): Promise<RegisterUserResponseDto> {
-    const payload = {
-      email: dto.email.toLowerCase(),
-      username: dto.username.trim(),
-      password: dto.password,
-    };
-
-    const user: User = await this.registerUserService.execute(payload);
-
-    return {
-      uuid: user.uuid,
-      username: user.username,
-      email: user.email,
-      createdAt: user.createdAt,
-      updatedAt: user.updatedAt,
-      deletedAt: user.deletedAt,
-    };
-  }
 
   @Get(':uuid')
   async findByUuid(@Param('uuid') uuid: string): Promise<FindUserByUuidResponseDto> {
