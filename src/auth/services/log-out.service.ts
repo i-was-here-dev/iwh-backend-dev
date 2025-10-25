8625179f470dd0f9ecfb3fcdb43f0529dbf3a9e2
Mantrimas <mantrimas.kasparavicius@gmail.com>
2025-10-25 18:15:31 +0300

    [IWH-66](feat): include duplication check in log out service
    included duplication error handling in log out service,
    exported blacklisted token saving in to seperate helper functions
    
    Ticket: https://iwashere.atlassian.net/browse/IWH-66

--- src/auth/services/log-out.service.ts
+++ src/auth/services/log-out.service.ts
@@ -1,3 +1,4 @@
+import { ConflictException } from '@nestjs/common';
 import { BlacklistedToken } from '../entities/blacklisted-token.entity';
 import { BlacklistedTokenRepositoryInterface } from '../repositories/blacklisted-token-repository.interface';
 import { LogOutPort, LogOutUseCase } from './usecases/log-out.usecase';
@@ -8,13 +9,23 @@ export class LogOutService implements LogOutUseCase {
   async execute(payload: LogOutPort): Promise<void> {
     const { refreshToken, validationToken } = payload;
 
-    const blackListedRefreshToken = new BlacklistedToken();
-    blackListedRefreshToken.token = refreshToken;
+    if ((await this.blacklistedTokenRepository.findByToken(validationToken)) || (await this.blacklistedTokenRepository.findByToken(refreshToken))) {
+      throw new ConflictException('Expired token');
+    }
 
+    await this.blacklistRefreshToken(refreshToken);
+    await this.blacklistValidationToken(validationToken);
+  }
+
+  private async blacklistRefreshToken(refreshToken: string) {
+    const blacklistedRefreshToken = new BlacklistedToken();
+    blacklistedRefreshToken.token = refreshToken;
+    await this.blacklistedTokenRepository.save(blacklistedRefreshToken);
+  }
+
+  private async blacklistValidationToken(validationToken: string) {
     const blacklistedValidationToken = new BlacklistedToken();
     blacklistedValidationToken.token = validationToken;
-
-    await this.blacklistedTokenRepository.save(blackListedRefreshToken);
     await this.blacklistedTokenRepository.save(blacklistedValidationToken);
   }
 }
