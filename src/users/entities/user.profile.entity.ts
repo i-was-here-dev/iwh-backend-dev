4c2329d2e13e3eb1d76a66f8826acaa7384ab102
ShuraTheSaint <dainiusknie@gmail.com>
2025-11-03 11:12:31 +0200

    chore: added columns: uuid, createdAt, deletedAt, updatedAt and cleaned up code

--- src/users/entities/user.profile.entity.ts
+++ src/users/entities/user.profile.entity.ts
@@ -1,10 +1,24 @@
-import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  Generated,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 import { User } from './user.entity';
 
-@Entity('profile')
+@Entity()
 export class Profile {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
+  @Generated()
+  uuid: string;
 
   @Column({ unique: true })
   nickname: string;
@@ -14,6 +28,15 @@ export class Profile {
 
   @Column()
   points: number;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: Date;
+
+  @UpdateDateColumn({ name: 'update_at' })
+  updatedAt: Date;
 
   @OneToOne(() => User, (user) => user.profile)
   @JoinColumn()
