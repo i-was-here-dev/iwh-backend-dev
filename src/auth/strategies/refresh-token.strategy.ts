02081621f3e57a6447c9c78a53345662b44bdc7b
Mantrimas <mantrimas.kasparavicius@gmail.com>
2025-11-05 21:25:18 +0200

    [IWH-81](feat): include blacklisted token check in refresh token guard
    improved refresh token guard by implementing blacklisted token existence
    check,
    
    limited refresh token error handling to a universal error message for
    any instance of failure
    
    Ticket: https://iwashere.atlassian.net/browse/IWH-81

--- src/auth/strategies/refresh-token.strategy.ts
+++ src/auth/strategies/refresh-token.strategy.ts
@@ -2,9 +2,11 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { RefreshTokenStrategyPayload } from '../types/refresh-token-strategy-payload.type';
 import { RefreshTokenStrategyResponse } from '../types/refresh-token-strategy-response.type';
+import { BlacklistedTokenRepositoryInterface } from '../repositories/blacklisted-token-repository.interface';
+import { UnauthorizedException } from '@nestjs/common';
 
 export class RefreshTokenStrategy extends PassportStrategy(Strategy, 'refresh-jwt') {
-  constructor() {
+  constructor(private readonly blacklistedTokenRepository: BlacklistedTokenRepositoryInterface) {
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([ExtractJwt.fromHeader('refresh-token')]),
       ignoreExpiration: false,
@@ -12,7 +14,12 @@ export class RefreshTokenStrategy extends PassportStrategy(Strategy, 'refresh-jw
     });
   }
 
-  async validate(payload: RefreshTokenStrategyPayload): Promise<RefreshTokenStrategyResponse> {
+  async validate(req: Request, payload: RefreshTokenStrategyPayload): Promise<RefreshTokenStrategyResponse> {
+    const token = ExtractJwt.fromExtractors([ExtractJwt.fromHeader('refresh-token')])(req);
+
+    const blacklistedToken = await this.blacklistedTokenRepository.findByToken(token);
+    if (blacklistedToken) throw new UnauthorizedException();
+
     return {
       userId: payload.sub,
       email: payload.email,
