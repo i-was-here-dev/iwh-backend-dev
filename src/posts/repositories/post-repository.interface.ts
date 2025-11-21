f8f10e9760cafafaf6240f9f1faa5b069df233e4
Mantrimas <mantrimas.kasparavicius@gmail.com>
2025-11-21 18:45:10 +0200

    [IWH-90](feat): implement find in vicinity method post repository
    implemented `findInVicinity` method in post repository which finds all
    the posts in user radius and returns paginated posts
    
    Ticket: https://iwashere.atlassian.net/browse/IWH-124

--- src/posts/repositories/post-repository.interface.ts
+++ src/posts/repositories/post-repository.interface.ts
@@ -1,10 +1,12 @@
 import { Post } from 'src/posts/entities/post.entity';
 import { BoundingBoxQuery } from '../interfaces/bounding-box-query.interface';
+import { PointLocation } from '../types/location.type';
 
 export interface PostRepositoryInterface {
   findByUserId(userId: number): Promise<Post[] | null>;
   findByUuid(uuid: string): Promise<Post | null>;
   findInBoundingBox(query: BoundingBoxQuery): Promise<Post[] | null>;
+  findInVicinity(pointLocation: PointLocation, vicinityDistance: number): Promise<Post[] | null>;
   save(post: Post): Promise<Post>;
   findByUuid(uuid: string): Promise<Post | null>;
   softDelete(post: Post): Promise<boolean>;
--- src/posts/repositories/postgres/post.repository.ts
+++ src/posts/repositories/postgres/post.repository.ts
@@ -2,6 +2,7 @@ import { Post } from 'src/posts/entities/post.entity';
 import { Between, Repository, UpdateResult } from 'typeorm';
 import { PostRepositoryInterface } from '../post-repository.interface';
 import { BoundingBoxQuery } from 'src/posts/interfaces/bounding-box-query.interface';
+import { PointLocation } from 'src/posts/types/location.type';
 
 export class PostRepository implements PostRepositoryInterface {
   constructor(private readonly repository: Repository<Post>) {}
@@ -42,7 +43,8 @@ export class PostRepository implements PostRepositoryInterface {
 
     return await this.repository
       .createQueryBuilder('post')
-      .leftJoinAndSelect('post.profile', 'profile')
+      .leftJoinAndSelect('post.user', 'user')
+      .leftJoinAndSelect('user.profile', 'profile')
       .where('post.latitude BETWEEN :minLatitude AND :maxLatitude', {
         minLatitude: minLatitude,
         maxLatitude: maxLatitude,
@@ -53,6 +55,38 @@ export class PostRepository implements PostRepositoryInterface {
       })
       .orderBy('RANDOM()')
       .take(query.limit)
+      .getMany();
+  }
+
+  async findInVicinity(pointLocation: PointLocation, vicinityDistance: number, page: number = 1, limit: number = 5): Promise<Post[] | null> {
+    const offset = (page - 1) * limit;
+
+    return await this.repository
+      .createQueryBuilder('post')
+      .leftJoinAndSelect('post.user', 'user')
+      .leftJoinAndSelect('user.profile', 'profile')
+      .leftJoin('post.approvals', 'approval')
+      .addSelect('COUNT(approval.id)', 'approvalCount')
+      .where(
+        `
+        6371000 * acos(
+          cos(radians(:latitude)) * 
+          cos(radians(post.latitude)) * 
+          cos(radians(post.longitude) - radians(:longitude)) + 
+          sin(radians(:latitude)) * 
+          sin(radians(post.latitude))
+        ) <= :distance
+      `,
+        {
+          latitude: pointLocation.latitude,
+          longitude: pointLocation.longitude,
+          distance: vicinityDistance, // distance in meters
+        },
+      )
+      .groupBy('post.id, user.id, profile.id')
+      .orderBy('approvalCount', 'DESC')
+      .skip(offset)
+      .take(limit)
       .getMany();
   }
 }
