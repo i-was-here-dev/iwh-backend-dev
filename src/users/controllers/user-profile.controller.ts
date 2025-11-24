6bb01bbf7d019623330dc3061f8e57fb4988b62b
Mantrimas <mantrimas.kasparavicius@gmail.com>
2025-11-24 16:00:55 +0200

    docs: include openapi documentation

--- src/users/controllers/user-profile.controller.ts
+++ src/users/controllers/user-profile.controller.ts
@@ -6,14 +6,14 @@ import { FindProfileByUserIdResponseDto } from '../dto/find-profile-by-user-id-r
 import { UserData } from 'src/auth/decorators/user-data.decorator';
 import { JwtAuthGuardResponse } from 'src/auth/interfaces/jwt-auth-guard-response.interface';
 
-@Controller('profile/me')
+@Controller('profile')
 export class ProfileController {
   constructor(
     @Inject(UsersDiTokens.FindProfileByUserIdService)
     private readonly findProfileByUserIdService: FindProfileByUserIdUseCase,
   ) {}
 
-  @Get()
+  @Get('me')
   async findByUserId(@UserData() user: JwtAuthGuardResponse): Promise<FindProfileByUserIdResponseDto> {
     const payload: FindProfileByUserIdPort = { id: user.id };
     const profile: UserProfile = await this.findProfileByUserIdService.execute(payload);
@@ -23,8 +23,19 @@ export class ProfileController {
       nickname: profile.nickname,
       points: profile.points,
       profilePictureName: profile.profilePictureName,
-      posts: profile.user.posts,
-      comments: profile.user.comments,
+      posts: profile.user.posts.map((post) => ({
+        uuid: post.uuid,
+        title: post.title,
+        body: post.body,
+        latitude: post.latitude,
+        longitude: post.longitude,
+        createdAt: post.createdAt,
+      })),
+      comments: profile.user.comments.map((comment) => ({
+        uuid: comment.uuid,
+        body: comment.body,
+        createdAt: comment.createdAt,
+      })),
       deletedAt: profile.deletedAt,
       createdAt: profile.createdAt,
       updatedAt: profile.updatedAt,
--- src/users/dto/find-profile-by-user-id-response.dto.ts
+++ src/users/dto/find-profile-by-user-id-response.dto.ts
@@ -1,13 +1,21 @@
-import { Comment } from 'src/posts/entities/comment.entity';
-import { Post } from 'src/posts/entities/post.entity';
-
 export class FindProfileByUserIdResponseDto {
   uuid: string;
   nickname: string;
   points: number;
   profilePictureName: string;
-  posts: Post[];
-  comments: Comment[];
+  posts: {
+    uuid: string;
+    title: string;
+    body: string;
+    latitude: number;
+    longitude: number;
+    createdAt: Date;
+  }[];
+  comments: {
+    uuid: string;
+    body: string;
+    createdAt: Date;
+  }[];
   deletedAt: Date;
   createdAt: Date;
   updatedAt: Date;
