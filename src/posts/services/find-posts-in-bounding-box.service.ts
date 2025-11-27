5de5caeb766d2cc9ca01d03bbe0729e09a2438f8
Mantrimas <mantrimas.kasparavicius@gmail.com>
2025-11-27 03:11:21 +0200

    docs: corrected posts/:uuid endpoint

--- src/posts/services/find-posts-in-bounding-box.service.ts
+++ src/posts/services/find-posts-in-bounding-box.service.ts
@@ -12,6 +12,9 @@ export class FindPostsInBoundingBoxService implements FindPostsInBoundingBoxUseC
   async execute(payload: FindPostsInBoundingBoxPort): Promise<Post[]> {
     const { latitude, longitude, boxWidth, boxLength } = payload;
 
+    console.log(latitude, longitude);
+    console.log(boxWidth, boxLength);
+
     const posts: Post[] = await this.postRepository.findInBoundingBox({
       centerLatitude: latitude,
       centerLongitude: longitude,
@@ -21,6 +24,8 @@ export class FindPostsInBoundingBoxService implements FindPostsInBoundingBoxUseC
     });
 
     if (!posts) throw new NotFoundException('Posts not found');
+
+    console.log(posts);
 
     return posts;
   }
